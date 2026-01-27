47647c3ea3cca3b972278804aaacc478b3e6cf71
luisztc <tczhang2006@163.com>
2026-01-27 11:29:41 +0100

    1

---
+++ Assets/ztc/Checkpointmanager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    public static CheckpointManager Instance;
+
+    private Vector3 checkpointPos;
+    private Quaternion checkpointRot;
+    private bool hasCheckpoint = false;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public void SetCheckpoint(Vector3 pos, Quaternion rot)
+    {
+        checkpointPos = pos;
+        checkpointRot = rot;
+        hasCheckpoint = true;
+
+        Debug.Log("Checkpoint saved");
+    }
+
+    public void Respawn(Rigidbody rb)
+    {
+        if (!hasCheckpoint) return;
+
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        rb.position = checkpointPos;
+        rb.rotation = checkpointRot;
+    }
+}
