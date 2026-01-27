096c8f8959c29de4071efd9008da68ba460413a9
luisztc <tczhang2006@163.com>
2026-01-27 13:37:31 +0100

    1

--- Assets/ztc/Pendulum.cs
+++ Assets/ztc/Pendulum.cs
@@ -1,26 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Pendulum : MonoBehaviour
 {
-	public float speed = 1f;
-	public float limit = 75f; //Limit in degrees of the movement
-	public bool randomStart = true; //If you want to modify the start position
-	private float random = 0;
+    public float speed = 1f;
+    public float limit = 75f;
+    public bool randomStart = true;
 
-	// Start is called before the first frame update
-	void Awake()
+    float random = 0f;
+    bool rotateOnX = false; // true = X轴摆，false = Z轴摆
+
+    void Awake()
     {
-		if(randomStart)
-			random = Random.Range(0f, 1f);
-	}
+        if (randomStart)
+            random = Random.Range(0f, 1f);
 
-    // Update is called once per frame
-	void Update()
-	{
-    	float angle = limit * Mathf.Sin((Time.time + random) * speed);
-    	transform.localRotation = Quaternion.Euler(0, 0, angle);
-	}
+        // 只在一开始判断一次
+        float yAngle = transform.localRotation.y;
+        if (yAngle > 180f) yAngle -= 360f;
 
+        rotateOnX = Mathf.Abs(yAngle) > 0.01f;
+    }
+
+    void Update()
+    {
+        float angle = limit * Mathf.Sin((Time.time + random) * speed);
+
+        if (rotateOnX)
+        {
+            transform.localRotation = Quaternion.Euler(0f, 90f, angle);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0f, 0f, angle);
+        }
+    }
 }
